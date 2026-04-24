9be01356b5125b3282d67174ce25c1df961aedd5
cole-koryto <colekoryto17@gmail.com>
2026-04-24 14:57:46 -0500

    homework4 part 1 draft

--- homework/homework4/include/metaprogramming.hpp
+++ homework/homework4/include/metaprogramming.hpp
@@ -9,4 +9,30 @@ template<> struct Factorial<0> {
     static constexpr int value = 1;
 };
 
-// Add more TMP utilities, traits, and enable_if functions here
+// TMP-based fibonacci
+template<int N> struct Fibonacci {
+    static constexpr int value = Fibonacci<N - 1>::value + Fibonacci<N - 2>::value;
+};
+template<> struct Fibonacci<1> {
+    static constexpr int value = 1;
+};
+template<> struct Fibonacci<0> {
+    static constexpr int value = 0;
+};
+
+// TMP-based odd check and print
+template<int N> struct IsOdd {
+    static constexpr bool value = (N % 2 != 0);
+};
+template<int N>
+std::enable_if_t<IsOdd<N>::value> print_if_odd()
+{
+    std::cout << N << std::endl;
+};
+
+// TMP-based print all
+template<typename... Args>
+void print_all(Args... args)
+{
+    ((std::cout << args << ", "), ...) << std::endl;  // fold expression
+};
--- homework/homework4/src/main.cpp
+++ homework/homework4/src/main.cpp
@@ -6,11 +6,15 @@
 #include "OrderBookBuffer.hpp"
 
 int main() {
-    std::cout << "🚀 HFT Template Homework Starter\n";
+    std::cout << "HFT Template Homework Starter\n";
 
-    // TODO: Fill with examples from each header
-    // Example:
-    // static_assert(Factorial<5>::value == 120, "Incorrect factorial");
+    // Tests metaprogramming
+    static_assert(Factorial<5>::value == 120, "Incorrect Factorial");
+    static_assert(Fibonacci<7>::value == 13, "Incorrect Fibonacci");
+    static_assert(IsOdd<1>::value, "Incorrect IsOdd");
+    print_if_odd<5>();
+    print_all(7, 8, 1);
+
 
     return 0;
 }
